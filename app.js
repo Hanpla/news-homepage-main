7112ff9fb08aa4ef25fe70f60217aa8bc2235770
Hanpla <115339955+Hanpla@users.noreply.github.com>
2023-04-22 21:54:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const menu = document.getElementById("menu_logo");
 const menuBox = document.querySelector(".menu_box");
 const menuClose = document.getElementById("menu_close");
+const body = document.querySelector("body");
 
 menu.addEventListener("click", () => {
     menuBox.style.display = "block";
+    body.style.backgroundColor = " rgb(219, 219, 219)";
 })
 menuClose.addEventListener("click", () => {
     menuBox.style.display = "none";
+    body.style.backgroundColor = "white";
 })
